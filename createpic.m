1b0913db2e09646d29fb4be6a42c3e0fa68d2732
GustavSchedin <35923950+GustavSchedin@users.noreply.github.com>
2018-02-15 09:09:59 +0100

    Add files via upload

---
+++ createpic.m
@@ -0,0 +1,8 @@
+% Tar in tre matriser av samma storlek och skapar en bild
+
+function [] = createpic(A,B,C)
+
+E = cat(3,A,B,C); %Sätter ihop tre matriser till en nxmx3-matris
+Y = uint8(E);
+imwrite(Y,'interpolpic.png')
+
