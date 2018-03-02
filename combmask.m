3edefa6c8c9127220640643bcca33c9d3610c285
GustavSchedin <35923950+GustavSchedin@users.noreply.github.com>
2018-03-03 00:07:46 +0100

    Update combmask.m

--- combmask.m
+++ combmask.m
@@ -1,5 +1,5 @@
 % Funktion som kombinerar 2 maskmatriser.
-% Maskmatriserna måste ha samma dimension.
+% Maskmatriserna mÃ¥ste ha samma dimension.
 
 % Tillsammans med funktionerna "mask_rect.m" och "bound_mask.m"
 % upfyller dessa "krav 10" i kravspecifikationen.
@@ -11,3 +11,4 @@ mask_B1 = mask_B == 0;
 mask_C = mask_A1 + mask_B1;
 
 comb_mask = mask_C == 0;
+end
