9e940b140e49411e6d23d3102d10ec80eb095d70
GustavSchedin <35923950+GustavSchedin@users.noreply.github.com>
2018-03-19 17:43:24 +0100

    Delete CreateMask.m

--- CreateMask.m
+++
@@ -1,8 +0,0 @@
-function [mask] = CreateMask(start, stop, size, mask_matrix)
-% Creates a mask matrix with zeroes between start and stop
-% Takes start stop size and a previous mask_matrix. For a completely new mask use CreateMask(start, stop, size, ones(1, size)
-	for i=start:1:stop
-		mask_matrix(i)=0;
-	end
-	mask = sparse(mask_matrix);
-end
