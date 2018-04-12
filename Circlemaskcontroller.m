9a8ac03d123a39c593c48b078cfada125f9338de
kribr883 <kribr883@student.liu.se>
2018-04-12 13:20:52 +0200

    Circlemaskcontroller.m

--- Circlemaskcontroller.m
+++ Circlemaskcontroller.m
@@ -1,10 +1,14 @@
 function[out]=Circlemaskcontroller(Xpoints, Ypoints, mask)
+if size(Xpoints,1)==0;
+    newmask=mask;
+else
 TwoXpoints=Xpoints(1:2,1);
 TwoYpoints=Ypoints(1:2,1);
 newmask=Circlemaskmaker(TwoXpoints, TwoYpoints, mask);
 if size(Xpoints, 1)>2
     newmask = combmask(newmask, Circlemaskcontroller(Xpoints(3:end), Ypoints(3:end), newmask));
 else newmask;
+end
 end
 out = newmask;
 end
