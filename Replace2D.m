fb57d0312552b534626326ea1c1e144427f77d8e
KristofferBrander <35964582+KristofferBrander@users.noreply.github.com>
2018-04-13 22:22:54 +0200

    Lade till kodkommentarer

--- Replace2D.m
+++ Replace2D.m
@@ -1,3 +1,10 @@
+%Tar en bild som argument och anropar intpol_2D tillsammans med bilden och
+%en kombinerad maskmatris av mask_rect med bilden som argument och
+%Circlemaskchooser med bilden som argument.
+%combmask kombinerar som så att om det finns en nolla på en viss plats i
+%någon av matriserna så blir det en nolla på den platsen. Om det på en viss 
+%plats i matriserna finns ettor i båda matriserna så blir det en etta.
+
 function[]=Replace2D(image)
 
 intpol_2D(image, combmask(mask_rect(image), Circlemaskchooser(image)))
