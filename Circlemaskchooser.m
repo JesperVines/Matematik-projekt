9980a26c19932c4277f01c702b9011a283c1af3a
KristofferBrander <35964582+KristofferBrander@users.noreply.github.com>
2018-04-13 22:24:29 +0200

    Lade till kodkommentarer

--- Circlemaskchooser.m
+++ Circlemaskchooser.m
@@ -1,3 +1,12 @@
+%Tillsammans med Circlemaskcontroller.m och Circlemaskmaker.m uppfyller
+%denna krav 11 i kravspecifikationen.
+
+%Tar en bild som argument. Öppnar bilden och låter användaren trycka på 
+%punkter på bilden. Anropar sedan Circlemaskcontroller med X- och
+%Y-koordinaterna för punkterna man klickade på i bilden, och en matris
+%med ettor med samma dimensioner som bildens pixeldimensioner. Returnerar
+%vad Circlemaskcontroller returnerar.
+
 function[out]=Circlemaskchooser(image)
 imshow(image,'InitialMagnification','fit')
 [X,Y] = ginput;
