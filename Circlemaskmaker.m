7b17995d42589bd8a33ab4143717fffdfab1a7b0
KristofferBrander <35964582+KristofferBrander@users.noreply.github.com>
2018-04-13 22:22:17 +0200

    Lade till kodkommentarer

--- Circlemaskmaker.m
+++ Circlemaskmaker.m
@@ -1,3 +1,18 @@
+%Tillsammans med Circlemaskcontroller.m och Circlemaskchooser.m uppfyller
+%denna krav 11 i kravspecifikationen.
+
+%Circlemaskmaker tar x-värdena och y-värdena av två punkter representerade 
+%som kolonnvektorer, och en maskmatris som argument. Den första punkten är 
+%alltså första värdet i Xpoints kombinerad med första värdet i Ypoints och 
+%den andra punkten blir andra värdet i Xpoints kombinerad med andra värdet 
+%i Ypoints.
+
+%Den tolkar sedan den första punkten som indexkoordinaterna i masken för
+%mitten av en cirkelskiva, och den andra punkten som indexkoordinaterna i
+%masken för en randpunkt på cirkelskivan. Den sätter sedan alla
+%indexkoordinater i maskmatrisen som är inre punkter i cirkelskivan till 0
+%och returnerar den nya masken.
+
 function[Amask]=Circlemaskmaker(Xpoints, Ypoints, mask)
 
 radius=(sqrt((Xpoints(1,1)-Xpoints(2,1))^2+(Ypoints(1,1)-Ypoints(2,1))^2));
