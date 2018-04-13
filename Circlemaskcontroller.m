c36a6a6ff05bf3a97c7748328b951227c719b0e6
KristofferBrander <35964582+KristofferBrander@users.noreply.github.com>
2018-04-13 22:23:37 +0200

    Update Circlemaskcontroller.m

--- Circlemaskcontroller.m
+++ Circlemaskcontroller.m
@@ -1,5 +1,18 @@
+%Tillsammans med Circlemaskchooser.m och Circlemaskmaker.m uppfyller
+%denna krav 11 i kravspecifikationen.
+
+%Circlemaskmaker tar x-värdena och y-värdena av några punkter, och en mask
+%som argument.
+
+%Den anropar sedan Circlemaskmaker med x- och y-värdena av de två första
+%punkterna och sin mask. Sedan anropar den Circlemaskmaker med x- och
+%y-värdena av den tredje och fjärde punkten (om de finns) tillsammans med
+%masken som den fick förra gången den körde circlemaskmaker. På detta sätt
+%fortsätter den tills den inte längre har två punkter att tillgå. Då
+%returnerar den masken som den senast fick från Circlemaskmaker.
+
 function[out]=Circlemaskcontroller(Xpoints, Ypoints, mask)
-if size(Xpoints,1)==0;
+if size(Xpoints,1)<2;
     newmask=mask;
 else
 TwoXpoints=Xpoints(1:2,1);
