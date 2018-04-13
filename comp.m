9e4238b512168829bc1b5999f3ab8052cb37cbc4
GustavSchedin <35923950+GustavSchedin@users.noreply.github.com>
2018-04-13 16:07:38 +0200

    Update comp.m

--- comp.m
+++ comp.m
@@ -1,17 +1,30 @@
+% Denna funktion reducerar antalet pixlar som behöver lagras
+% i en bild för att återskapa den genom interpolering.
 
+% Funktionen tar som input en bildfil samt en tolerans.
+% Toleransen är ett numeriskt värde >= 0, ett större värde
+% komprimerar bilden mer.
+
+% Ett korrekt anrop med bilden "testbild.png" och toleransen 150
+% är: comp('testbild.png',150)
+
+% Output är en 3-dimensionell maskmatris.
 function [mask_3D] = comp(image,tol)
 
 A = imread(image);
 S = double(A);
 
+% Uppdelning av bilden i RGB-kanaler.
 R = S(:,:,1);
 G = S(:,:,2);
 B = S(:,:,3);
 
+% Komprimerar över RGB-kanalerna.
 RI = func01(R,tol);
 GI = func01(G,tol);
 BI = func01(B,tol);
 
+% Sätter ihop maskmatriserna till en 3-dimensionell matris.
 mask_3D = cat(3,RI,GI,BI);
 
 function [mask] = func01(img_mat,tol)
