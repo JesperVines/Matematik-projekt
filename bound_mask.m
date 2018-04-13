1a29611a8a308e43325005ca122fc2c16b94128b
GustavSchedin <35923950+GustavSchedin@users.noreply.github.com>
2018-04-13 16:15:01 +0200

    Update bound_mask.m

--- bound_mask.m
+++ bound_mask.m
@@ -1,19 +1,17 @@
 % Funktionen tar in en bildfil samt en maskmatris och visar
-% bildfilen i ett fönster med en kant ritad enligt maskmatrisen.
-% Denna funktion, tillsammans med funktionerna "mask_rect.m" och 
-% "combmask.m", uppfyller "krav 10" i kravspecifikationen.
+% bildfilen i ett fÃ¶nster med en kant ritad enligt maskmatrisen.
 function [] = bound_mask(image,mask)
 
-% Gör alla 1:or i maskmatrisen till 0:or och tvärtom.
+% GÃ¶r alla 1:or i maskmatrisen till 0:or och tvÃ¤rtom.
 mask_inv = mask == 0;
 
 % Tar en sparse-matris och konverterar den till en full matris.
 S = full(mask_inv);
 
-% Beräknar områden som består av 1:or.
+% BerÃ¤knar omrÃ¥den som bestÃ¥r av 1:or.
 bound = bwboundaries(S);
 
-% Ritar in en kant längs områdena i maskmatrisen.
+% Ritar in en kant lÃ¤ngs omrÃ¥dena i maskmatrisen.
 imshow(image)
 hold on
 visboundaries(bound)
